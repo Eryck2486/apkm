912813e2798511279ccafe7b36da6336fb89a0db
Eryck <eryck.rodrigues31@gmail.com>
2026-04-13 19:57:44 -0300

    Correção do script de build

--- build_and_send.sh
+++ build_and_send.sh
@@ -1,4 +1,12 @@
 
 #!/bin/bash
+#Obtendo os argumentos de arquitetura e nível da API
+ARCHITECTURE=$1
+ANDROIDAPILEVEL=$2
+if [ -z "$ARCHITECTURE" ] || [ -z "$ANDROIDAPILEVEL" ]; then
+    echo "Uso: $0 <arquitetura> <nível da API>"
+    echo "Exemplo: $0 arm64-v8a 30"
+    exit 1
+fi
 adb root && 
-make build_apkm && adb push build/apkm-module-* /sdcard/apkm-module-installer.zip && adb shell "magisk --install-module /sdcard/apkm-module-installer.zip" && adb shell reboot
+make build arch=${ARCHITECTURE} api=${ANDROIDAPILEVEL} && adb push build/apkm-module-${ARCHITECTURE}-API${ANDROIDAPILEVEL}.zip /sdcard/apkm-module-installer.zip && adb shell "magisk --install-module /sdcard/apkm-module-installer.zip" && adb shell reboot
